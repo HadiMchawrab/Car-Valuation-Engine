89c727f3f023534dabd60486810795d7b71331fd
LightAwesome <welcometouseef@gmail.com>
2025-07-16 18:24:01 +0400

    Added Syarah Crawler, Carswicth, and the corresponding daily crawlers.

---
+++ scrapers/scraper/spiders/syarah.py
@@ -0,0 +1,10 @@
+from .syarah_template import SyarahTemplateSpider
+
+
+class SyarahSpider(SyarahTemplateSpider):
+  name = 'syarah'
+  custom_settings = {
+    **SyarahTemplateSpider.custom_settings,
+      "FEEDS": {"syarah.json": {"format": "json", "encoding": "utf8", "overwrite": True}},
+       "ITEM_PIPELINES": {"scraper.pipelines.SyarahPostgresPipeline": 300},
+  }
